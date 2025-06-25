4b468053bfe77edd63c34e66a51ebf828975e1f8
s19835 <s19835@sci.pdn.ac.lk>
2025-06-25 14:23:53 +0530

    add: parent folder

---
+++ src/main/java/com/s19835/jobqueueoptimizer/JobQueueOptimizerApplication.java
@@ -0,0 +1,13 @@
+package com.s19835.jobqueueoptimizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobQueueOptimizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobQueueOptimizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/s19835/jobqueueoptimizer/JobQueueOptimizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s19835.jobqueueoptimizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQueueOptimizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
