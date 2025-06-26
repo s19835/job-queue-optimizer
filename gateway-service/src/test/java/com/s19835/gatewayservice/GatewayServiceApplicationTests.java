4e79d564dc3a8e6bce5542f104785f0316c86c96
s19835 <s19835@sci.pdn.ac.lk>
2025-06-26 14:58:48 +0530

    test: api doc & fallback endpoint

--- gateway-service/src/test/java/com/s19835/gatewayservice/GatewayServiceApplicationTests.java
+++ gateway-service/src/test/java/com/s19835/gatewayservice/GatewayServiceApplicationTests.java
@@ -1,13 +1,26 @@
 package com.s19835.gatewayservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GatewayServiceApplicationTests {
 
+    @Autowired
+    private WebTestClient webTestClient;
+
     @Test
-    void contextLoads() {
+    void testApiDocumentationEndpoint() {
+        webTestClient.get().uri("/api-docs").exchange().expectStatus().isOk()
+                .expectBody().jsonPath("$.jobs_endpoint").exists();
+    }
+
+    @Test
+    void testFallbackEndpoint() {
+        webTestClient.get().uri("/fallback/jobs").exchange().expectStatus().is5xxServerError()
+                .expectBody(String.class).isEqualTo("Job Service is currently unavailable. Please try again later.");
     }
 
 }
