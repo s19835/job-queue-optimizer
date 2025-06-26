ebbc6b4b930b8b2b6de5db7d92043d4427efae33
s19835 <s19835@sci.pdn.ac.lk>
2025-06-27 01:34:01 +0530

    fix: module creation task schedule service name no other names

---
+++ task-scheduler-service/src/main/java/com/s19835/taskschedulerservice/TaskSchedulerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.s19835.taskschedulerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerServiceApplication.class, args);
+    }
+
+}
---
+++ task-scheduler-service/src/test/java/com/s19835/taskschedulerservice/TaskSchedulerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s19835.taskschedulerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
