3a5514d4bb132e8032e220f7e70771c20fb73a5e
s19835 <s19835@sci.pdn.ac.lk>
2025-06-27 16:52:01 +0530

    add: shared module

---
+++ shared/src/test/java/com/s19835/shared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s19835.shared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
